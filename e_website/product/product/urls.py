49324b0f3173a7025446cba53361753b187c6204
JagadeeshSurthani <surthanijagadeesh@gmail.com>
2025-06-25 09:17:10 +0530

    application.settings

--- e_website/product/product/urls.py
+++ e_website/product/product/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for e_website project.
+URL configuration for product project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path,include
+from django.contrib import views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('product/', include('product.urls')),
+urlpatterns= [
+    path('', views.home, name='home'),
 ]
+
+
